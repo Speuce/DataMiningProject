dcc63af789dca7a03b6de734da3d45ea15301887
Speuce <mrk5199@gmail.com>
2021-11-06 17:45:23 -0500

    Update data_structs.py

--- src/data_structs.py
+++ src/data_structs.py
@@ -1,4 +1,4 @@
-from typing import List, Set, Optional
+from typing import List, Set, Optional, Tuple
 
 import numpy as np
 
@@ -13,10 +13,10 @@ class TreeNode:
         self.update_bit_string()
 
     def update_bit_string(self):
-        bit_array = np.zeros(256)
+        bit_array = np.zeros(256, dtype=int)
         for bin_index in self.bin_index_vals:
             bit_array[bin_index] = 1
-        self.select_array = np.packbits(bit_array, axis=-1).view(np.uint32)
+        self.select_array = np.packbits(bit_array).view(np.uint32)
 
     def add_binary_indicies(self, indicies: Set[int]):
         self.bin_index_vals.update(indicies)
@@ -26,7 +26,7 @@ class TreeNode:
 class Tree:
     def __init__(self, branch_factor: int, verbose_name: str, levels: int = 2):
         self.branching_factor = branch_factor
-        self.node_list: List[Optional[TreeNode]] = [None] * ((branch_factor ** levels) + 1)
+        self.node_list: List[Optional[TreeNode]] = [None] * ((branch_factor ** (levels-1)) + 1)
         self.node_list[0] = TreeNode(index=0, bin_index_vals=set(), verbose_name="ALL" + verbose_name)
 
     def remove_node(self, to_remove: TreeNode):
@@ -54,3 +54,14 @@ class Tree:
             return []
         else:
             return [x for x in self.node_list[first_child_index: last_child_index + 1] if x]
+
+
+def create_basic_tree(values: List[Tuple[int, str]], verbose_name: str) -> Tree:
+    """
+    Creates a very basic 2 level tree with 1 ALL_x node at the top,
+    and all other nodes (indicated by the list of tuples provided) in the second level
+    """
+    the_tree = Tree(len(values), verbose_name)
+    for i, (binary_index, name) in enumerate(values):
+        the_tree.add_node(i+1, {binary_index}, verbose_name + "_" + name)
+    return the_tree
