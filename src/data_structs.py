f927dadcd784ee53c9cd0f7bb1433be274160726
Speuce <mrk5199@gmail.com>
2021-11-06 19:06:20 -0500

    Update data_structs.py

--- src/data_structs.py
+++ src/data_structs.py
@@ -28,11 +28,15 @@ class Tree:
         self.branching_factor = branch_factor
         self.node_list: List[Optional[TreeNode]] = [None] * ((branch_factor ** (levels-1)) + 1)
         self.node_list[0] = TreeNode(index=0, bin_index_vals=set(), verbose_name="ALL" + verbose_name)
+        self.next_node = 1
 
     def remove_node(self, to_remove: TreeNode):
         pass
 
-    def add_node(self, index: int, bin_index_vals: Set[int], verbose_name: str):
+    def add_node(self, bin_index_vals: Set[int], verbose_name: str):
+        self.set_node(self.next_node, bin_index_vals, verbose_name)
+
+    def set_node(self, index: int, bin_index_vals: Set[int], verbose_name: str):
         new_node = TreeNode(index, bin_index_vals, verbose_name)
         self.node_list[index] = new_node
         # update parent binary lists
@@ -40,6 +44,8 @@ class Tree:
         while curr:
             curr.add_binary_indicies(new_node.bin_index_vals)
             curr = self.get_parent_node(curr)
+        if index >= self.next_node:
+            self.next_node = index+1
 
     def get_parent_node(self, curr: TreeNode) -> Optional[TreeNode]:
         parent_index = (curr.index - 1) // self.branching_factor
@@ -55,6 +61,9 @@ class Tree:
         else:
             return [x for x in self.node_list[first_child_index: last_child_index + 1] if x]
 
+    def __str__(self):
+        return self.node_list[0].verbose_name[3:] + " Tree"
+
 
 def create_basic_tree(values: List[Tuple[int, str]], verbose_name: str) -> Tree:
     """
@@ -63,5 +72,5 @@ def create_basic_tree(values: List[Tuple[int, str]], verbose_name: str) -> Tree:
     """
     the_tree = Tree(len(values), verbose_name)
     for i, (binary_index, name) in enumerate(values):
-        the_tree.add_node(i+1, {binary_index}, verbose_name + "_" + name)
+        the_tree.add_node({binary_index}, verbose_name + "_" + name)
     return the_tree
