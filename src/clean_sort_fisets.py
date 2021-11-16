87c7e59c3e0d5ce786ba60d8ce11caab61096a54
Speuce <mrk5199@gmail.com>
2021-11-16 09:33:04 -0600

    sort results by count

--- src/clean_sort_fisets.py
+++ src/clean_sort_fisets.py
@@ -1,4 +1,4 @@
-def clean_sort_itemsets(filepath: str, output_filepath:str):
+def clean_sort_itemsets(filepath: str, output_filepath: str):
     rules = 0
     duplicates = 0
     lines = []
@@ -11,9 +11,11 @@ def clean_sort_itemsets(filepath: str, output_filepath:str):
                 rules += 1
     print(f'Found {rules} frequent itemsets with {duplicates} duplicates')
     lines.sort()
+    lines = sorted(lines, key=lambda x: int(x.split('|')[1]))
     with open(output_filepath, "w") as text_file:
         for line in lines:
             text_file.write(line)
     print('done!')
 
-clean_sort_itemsets('output.txt', 'sorted.txt')
+
+clean_sort_itemsets('output.txt', 'sorted_by_count.txt')
