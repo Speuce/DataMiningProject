d16d2de371fe83984e679f76ccb64c60b32c8cde
Speuce <mrk5199@gmail.com>
2021-12-01 19:08:01 -0600

    Create rule_analyze_other.py

---
+++ src/rule_analyze_other.py
@@ -0,0 +1,67 @@
+import itertools
+
+
+class Rule:
+    def __init__(self, precedent, preSupport):
+        self.precedent = precedent
+        self.anticedent = list()
+        self.totalsupport = 0.0
+        self.preSupport = preSupport
+        self.confidence = 0.0
+
+    def calculateConfidence(self):
+        self.confidence = self.totalsupport / self.preSupport
+
+    def getPre(self):
+        return self.precedent
+
+    def getConf(self):
+        return self.confidence
+
+    def addAnti(self, anti, support):
+        self.anticedent = anti
+        self.totalsupport = support
+        self.calculateConfidence()
+
+    def printRule(self):
+        for x in self.precedent:
+            output.write(x + ",")
+        output.write(" -> ")
+        for x in self.anticedent:
+            output.write(x + ",")
+        output.write(" with confidence " + str(self.confidence) + "\n")
+
+
+def compare(first, second):
+    if first == second:
+        return True
+    else:
+        return False
+
+
+ruleList = list()
+singles = list()
+pre = list()
+preSupport = 0
+anti = list()
+antiSupport = 0
+support = 0
+currLine = 0
+
+type = 'serious'
+
+with open(f'../result/{type}_accidents.txt', 'r') as inputFile, open(f'../result/rules/2/{type}_accident_rules2.txt',
+                                                               'w') as output:
+    inp = inputFile.readlines()
+    for line in inp:
+        line = line.split('|')
+        support = line.pop()
+        newRule = Rule(line[0].strip(',').split(","), int(support))
+        # create precident rule with the target line
+        for newLine in inp:
+            newLine = newLine.split('|')
+            newSupport = newLine.pop()
+            if line[0] in newLine[0] and line[0] != newLine[0]:
+                antiRules = list(set(newLine[0].strip(',').split(',')) - set(newRule.precedent))
+                newRule.addAnti(antiRules, int(newSupport))
+                newRule.printRule()
